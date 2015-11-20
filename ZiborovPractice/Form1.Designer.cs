a77fa86b4fec68d8d5f4811ef587096a0c8ebbec
TimRauf <rauftim@gmail.com>
2015-11-20 15:04:38 +0300

    Beta

--- ZiborovPractice/Form1.Designer.cs
+++ ZiborovPractice/Form1.Designer.cs
@@ -32,12 +32,14 @@
             this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(34, 265);
+            this.label1.Location = new System.Drawing.Point(43, 59);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(35, 13);
             this.label1.TabIndex = 1;
@@ -45,19 +47,37 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(231, 251);
+            this.button1.Location = new System.Drawing.Point(119, 173);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(65, 27);
+            this.button1.Size = new System.Drawing.Size(138, 41);
             this.button1.TabIndex = 2;
-            this.button1.Text = "button1";
+            this.button1.Text = "Вычислить";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(47, 129);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label2";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(88, 126);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(169, 20);
+            this.textBox1.TabIndex = 4;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(433, 337);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
@@ -72,6 +92,8 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- ZiborovPractice/Form1.cs
+++ ZiborovPractice/Form1.cs
@@ -14,17 +14,29 @@ namespace ZiborovPractice
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            this.Text = "Test the ToolTip";
-            label1.Text = "Test test";
+            this.Text = "Print Greek letters";
+            label1.Text = String.Format(" Надйем длину окружности {0} = 2{1}{2}{1}R",Convert.ToChar(0x3B2), Convert.ToChar(0x2219),
+                Convert.ToChar(0x30), Math.PI);
             button1.Text = "Testing...";
             toolTip1.SetToolTip(button1, "Test phrase");
             toolTip1.IsBalloon = true;
+
+            label2.Text = "Enter the radius";
+            textBox1.Clear();
             
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            label1.Visible = !label1.Visible;
+            Single R;
+            var isCypher = Single.TryParse(textBox1.Text,System.Globalization.NumberStyles.Number,System.Globalization.NumberFormatInfo.CurrentInfo,out R);
+            if (isCypher == false)
+            {
+                MessageBox.Show("Вводите цифры!!!");
+            }
+            var beta = 2 * Math.PI * R;
+            MessageBox.Show(String.Format("Длина окружности {0} = {1:F4}",Convert.ToChar(0x3B2),beta),"Греческая буква");
+
         }
     }
 }
