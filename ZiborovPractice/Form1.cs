6a47a7f3ca3225564f2c1e132145d73cbd4004ad
TimRauf <rauftim@gmail.com>
2015-11-20 11:21:49 +0300

    Извлечение корня

--- ZiborovPractice/Form1.cs
+++ ZiborovPractice/Form1.cs
@@ -17,9 +17,32 @@ namespace ZiborovPractice
             InitializeComponent();
         }
 
-        private void label1_MouseHover(object sender, EventArgs e)
+        /*private void label1_MouseHover(object sender, EventArgs e)
         {
-            MessageBox.Show("Done it!\nGreat!!!");
+            MessageBox.Show("Done it!\nGreat!!!!");
+        }*/
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            base.Text = "Извлечение квадр.корня";
+            button1.Text = "Извлечь корень";
+            textBox1.Clear();
+            label1.Text = null;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Single X;
+            bool isItCypher = Single.TryParse(textBox1.Text,System.Globalization.NumberStyles.Number,System.Globalization.NumberFormatInfo.CurrentInfo,out X);
+            if (isItCypher == false)
+            {
+                label1.Text = "please enter a number";
+                label1.ForeColor = Color.Red;
+                return;
+            }
+            var Y = (Single)Math.Sqrt(X);
+            label1.ForeColor = Color.Black;
+            label1.Text = String.Format("Корень из {0} равен {1:F5}",X,Y);
         }
     }
 }
