e12a426f79b076812709b89bc1e90620c6f5fae1
TimRauf <rauftim@gmail.com>
2015-12-11 07:50:29 +0300

    a commit message

--- ZiborovPractice/Form1.Designer.cs
+++ ZiborovPractice/Form1.Designer.cs
@@ -30,11 +30,12 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(31, 209);
+            this.button1.Location = new System.Drawing.Point(56, 163);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(110, 67);
             this.button1.TabIndex = 0;
@@ -44,23 +45,34 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(278, 209);
+            this.button2.Location = new System.Drawing.Point(254, 163);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(108, 67);
             this.button2.TabIndex = 1;
             this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = true;
             // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(56, 60);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(306, 60);
+            this.textBox1.TabIndex = 2;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(433, 337);
+            this.ClientSize = new System.Drawing.Size(433, 263);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "ZiborovApps";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -68,6 +80,7 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- ZiborovPractice/Form1.cs
+++ ZiborovPractice/Form1.cs
@@ -6,6 +6,7 @@ namespace ZiborovPractice
 {
     public partial class Form1 : Form
     {
+        string fName;
         public Form1()
         {
             InitializeComponent();
@@ -13,14 +14,46 @@ namespace ZiborovPractice
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Произошло событие \"щелчок на первой кнопке\"");
+            try
+            {
+                var reader = new System.IO.StreamReader(fName);
+                textBox1.Text = reader.ReadToEnd();
+                reader.Close();
+
+            }
+            catch (System.IO.FileNotFoundException situate)
+            {
+                MessageBox.Show(situate.Message + "\n" + "Нет такого файла", "ОШибка", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                //throw;
+            }
+            catch (Exception situate)
+            {
+                MessageBox.Show(situate.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+
         }
         private void button2_Click(object sender, EventArgs e)
         {
-            //button1.PerformClick();
-            //button1_Click(null, null);
-            button1_Click(button1, EventArgs.Empty);
+            try
+            {
+                var writer = new System.IO.StreamWriter(fName, false);
+                writer.Write(textBox1.Text);
+                //writer.Close();
+                System.IO.File.WriteAllText(@"C:\Temp\11.txt", textBox1.Text);
+            }
+            catch (Exception situate)
+            {
+                MessageBox.Show(situate.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+        }
 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox1.Multiline = true; textBox1.Clear();
+            button1.Text = "Open"; button1.TabIndex = 0;
+            button2.Text = "Save";
+            this.Text = "Unicode here";
+            fName = @"C:\Temp\11.txt";
         }
     }
 }
