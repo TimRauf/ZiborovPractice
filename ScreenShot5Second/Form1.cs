9c4962f3d49698f20b7ff3df54ddfc3e0da6861f
TimRauf <rauftim@gmail.com>
2016-02-05 11:55:33 +0300

    1

---
+++ ScreenShot5Second/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ScreenShot5Second
+{
+    public partial class Form1 : Form
+    {
+        int i;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            i = 0;
+            this.Text = "Запись каждые 5 секунд в файл";
+            button1.Text = "Пуск";
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            i +=1;
+            this.Text = String.Format("Прошло {0} секунд", i);
+            if (i>=28)
+            {
+                timer1.Enabled = false; this.Close();
+
+            }
+            Single mod = i % 5;
+            if (mod != 0) return;
+            SendKeys.Send("%{PRTSC}");
+            var receiver = Clipboard.GetDataObject();
+            if (receiver.GetDataPresent(DataFormats.Bitmap) == true)
+            {
+                var obj = receiver.GetData(DataFormats.Bitmap);
+                var rastr = (Bitmap)obj;
+                var FileName = String.Format(@"C:\Temp\Pic{0}.bmp", i / 5);
+                rastr.Save(FileName);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Text = String.Format("Прошло 0 секунд");
+            timer1.Interval = 1000;
+            timer1.Enabled = true;
+        }
+    }
+}
---
+++ TableTxt/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TableTxt
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            const string NL = "\r\n";
+            this.ClientSize = new Size(438, 272);
+            this.textBox1.Size = new Size(415, 229);
+            this.Text = "ФОрмирование таблицы";
+            string[] Names = {"Андрей-раб", "Света-Х", "ЖЭК", "Справка по тел", "Алек Степанович",
+                "Мама - дом", "Карапузова Таня", "Театр"};
+            string[] Tel = { "2-22-45", "2-54-89", "2-65-89", "2-54-77", "2-10-90", "112", "900", "4-78998787" };
+            textBox1.ScrollBars = ScrollBars.Vertical;
+            textBox1.Font = new Font("Courier New", 9.0F);
+            textBox1.Text = "Таблица Телефонов" + NL + NL;
+            var i = 0;
+            foreach (var Name in Names)
+            {
+                textBox1.Text += String.Format("{0, -21} {1, -21}" + NL, Name, Tel[i]);
+                i += 1;
+            }
+            textBox1.Text += NL + "Примечание:" + NL + "для корректного отображения таблицы" +
+                NL + "в Блокноте укажите шрифт Courier New";
+            var writer = new System.IO.StreamWriter(@"C:\Temp\TestTable.txt", false, System.Text.Encoding.GetEncoding(1251));
+            writer.Write(textBox1.Text);
+            writer.Close();
+        }
+
+        private void показатьТаблицуToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                System.Diagnostics.Process.Start("Notepad", @"C:\Temp\TestTable.txt");
+            }
+            catch (Exception sit)
+            {
+                MessageBox.Show(sit.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                
+            }
+        }
+
+        private void выходToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
