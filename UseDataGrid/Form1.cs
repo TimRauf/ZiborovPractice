f3f22c39861cf8a9b1e62947f8d8239f21c585c5
TimRauf <rauftim@gmail.com>
2016-02-08 22:23:16 +0300

    123

---
+++ UseDataGrid/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UseDataGrid
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Text = "Формирование таблицы";
+            String[] Names =
+            {
+                "Andrew - work", "Bob - pizza", "Alice",
+                "George", "JamesBond", "Ann", "Tomhardware", "Orta"
+            };
+            String[] Phones =
+            {
+                "(555)123789", "4589775", "48738979324",
+                "(8552)3-10-45", "123", "456", "234424", "232333"
+            };
+            var Table = new DataTable();
+            Table.Columns.Add("NAMES");
+            Table.Columns.Add("PHONES");
+            for (int i = 0; i <= 8; i++)
+            {
+                Table.Rows.Add(Names[i], Phones[i]);
+            }
+            dataGridView1.DataSource = Table;
+        }
+    }
+}
